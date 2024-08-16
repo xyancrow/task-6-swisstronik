a61bc0d02fba4dfa6613adeea3191623a484218c
xyan <97204702+xyancrow@users.noreply.github.com>
2024-08-16 22:14:48 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xD2f46fFC2452b0e5cD0888e878D466C7eaa9A0d3'
-export const ProxyAdmin = '0x10F5E731Fb8dBf713f90Ac19A6d1744E60Eeb0d2'
-export const SWTRImplementation = '0x2734d2A639109e2d337F1502c52100cfADdDd948'
+export const SWTRProxy = '0x4B41EcbD2dFd7e6f772fd61E9F170353c4a78290'
+export const ProxyAdmin = '0x8595d1E976d581E6aba9F9d02D650AeeF37beE70'
+export const SWTRImplementation = '0x7084Fd66856Cf8Cd6738A91e919b0312894a6E7b'
     
